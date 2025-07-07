1074c8128bc83a22417bd46f10acc19fc50f4fa0
Alexandre CAROL <sasandre10@gmail.com>
2025-07-07 11:46:24 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const path = require('path')
+
+const app = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.static(path.join(__dirname, '../public')))
+
+const formRoutes = require('./routes/form').router
+app.use('/', formRoutes)
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
+}
+
+module.exports = app
